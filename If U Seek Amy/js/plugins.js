c8752f6745158f6e51c103ccb73011ceef9821fd
Raphaëll <raphaelle.mp@gmail.com>
2022-05-31 05:58:41 -0400

    The joke scene, progress on Mona Lisa

--- If U Seek Amy/js/plugins.js
+++ If U Seek Amy/js/plugins.js
@@ -4,5 +4,6 @@ var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"GALV_RollCredits","status":true,"description":"(v.1.5) A plugin that calls a new scene to display scrolling information located in an external text file.","parameters":{"Folder":"data","Skippable":"true","Block Skipping":"true","Title Menu":"Credits","Title Credits Music":"Circus_80sRemix"}},
-{"name":"NE_KeyboardWindow","status":true,"description":"v1.1.0 This allows to create a keyboard compatible window;","parameters":{"Allowed charset":"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?=()/&%$\"!^<>-.,;0123456789+-/*\\"}}
+{"name":"NE_KeyboardWindow","status":true,"description":"v1.1.0 This allows to create a keyboard compatible window;","parameters":{"Allowed charset":"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?=()/&%$\"!^<>-.,;0123456789+-/*\\"}},
+{"name":"YEP_MoveRouteCore","status":true,"description":"v1.04 Expand the number of movement options for the move\nroute events using this plugin.","parameters":{}}
 ];
